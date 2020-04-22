b57b4f836f47ee0d5104415a16d0e31cf6c5aba4
Marcos J Ribeiro <mjribeiro@usp.br>
2020-04-22 19:46:42 -0300

    atualizei a pasta

--- multiplier_income.py
+++ multiplier_income.py
@@ -8,7 +8,7 @@ Created on Sat Mar 14 17:52:58 2020
 
 
 
-def pib(c0, c1, t0=0, t1=0, t=0, b0=0, b1=0, i=0):
+def pib(c0, c1, g, t0=0, t1=0, t=0, b0=0, b1=0, i=0):
     '''
     c0: consumo autônomo
     c1: propensão marginal a consumir
@@ -19,13 +19,8 @@ def pib(c0, c1, t0=0, t1=0, t=0, b0=0, b1=0, i=0):
     b1: propensão a investir
     i: investimento
     '''
-    global y
-    
-    g = 150
-    i = 150   
-    
-    y = ( 1/(1 - c1 + c1*t1) )*( c0 + i + g - c1*t0 - c1*t)
-    
+    global y        
+     
     if t == 0:
         T = t0 + t1*y
     else:
@@ -35,33 +30,31 @@ def pib(c0, c1, t0=0, t1=0, t=0, b0=0, b1=0, i=0):
         I = b0 + b1*y
     else:
         I = i
-        
-    print('\033[1;033m')
-    print('     ')
+    
+    y = ( 1/(1 - c1 + c1*t1 - b1) )*(c0 - c1*t0 + b0 + g + i - c1*t)
+       
+         
+    print('\033[1;033m')    
     print('{:*^40}'.format('Multiplicador da renda'))
+    print('     ')
     print(f'\033[1;033mA renda de equilíbrio é igual a: {y:.2f}')
     print(f'Os impostos são iguais a: {T:.2f}')
     print(f'Os investimento são iguais a: {I:.2f}')
     print(f'A renda disponível é igual a: {(y-T):.2f}')
-    print(f'O multiplicador é igual a: {( 1/(1 - c1 + c1*t1) ):.2f}')
-    print(f'O consumo será: {(c0 +c1*y):.2f}')
+    print(f'O multiplicador é igual a: {( 1/(1 - c1 + c1*t1 - b1) ):.2f}')
+    print(f'O consumo será: {(c0 +c1*(y-T)):.2f}')
     print('  ')
     print('{:*^40}'.format('Fim do programa'))
             
 
 
-pib(160, 0.6, 0, 0, 100, 120, 0.8, 0)
+pib(c0=179, c1=0.4, t=100, b0=120, b1=0.2, g=110)
 
 
 
 
 
-
-
-
-
-
-
+1000**(3/4)*1000**(1/4) 
 
 
 
